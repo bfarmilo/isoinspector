59366e1cc40c7d92e6df036d3f243db041ba5e1d
Bill Farmilo <bill@farmilostrategy.com>
2018-08-20 14:09:41 -0400

    basic error message to console. Implemented manual mode switching

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -2,15 +2,20 @@ import { h } from 'preact';
 import { Link } from 'preact-router/match';
 import style from './style';
 
-const Header = () => (
+const Header = (props) => {
+	const names = {
+		webm: 'webM',
+		isobmff: 'ISOBMFF',
+		m2ts: 'MPEG-2 Transport Stream'
+	}
+	return (
 	<header class={style.header}>
-		<h1>ISO Inspector</h1>
+		<h1>{names[props.mode]} Inspector</h1>
 		<nav>
 			<Link activeClassName={style.active} href="/">Enter Data</Link>
-			<Link activeClassName={style.active} href="/profile">Me</Link>
-			<Link activeClassName={style.active} href="/profile/bill">Bill</Link>
+			<Link activeClassName={style.active} href="#" onClick={props.changeMode}>Change Decode Mode</Link>
 		</nav>
 	</header>
-);
+)};
 
 export default Header;
