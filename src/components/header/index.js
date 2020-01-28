d8481399a3ec69df208117619c95ac5a97b4140e
Bill Farmilo <bill@farmilostrategy.com>
2020-01-27 21:38:16 -0500

    improved styling, mpeg2 controls

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -7,12 +7,17 @@ const Header = (props) => {
 		mp4: 'ISOBMFF',
 		ts: 'MPEG-2 Transport Stream'
 	}
+
 	return (
 		<header class={style.header}>
 			<h1>Media Inspector</h1>
 			<nav>
-				<input type="file" style={{ opacity: 0 }} id="getFile" onChange={props.handleFiles} />
-				<label for="getFile"><a>Select Local File</a></label>
+				<input type="file" id="getFile" onChange={e => props.handleFiles(e, 'segment')} />
+				<label for="getFile"><a>Select Media File</a></label>
+				<input type="file" disabled={props.mode !== 'MP2T'} id="getPlayList" onChange={e => props.handleFiles(e, 'playlist')} />
+				<label for="getPlayList"><a style={props.mode !== 'MP2T' ? { color: 'grey' } : {}}>Select Playlist File</a></label>
+				<input type="file" disabled={props.mode !== 'MP2T'} id="getKey" onChange={e => props.handleFiles(e, 'key')} />
+				<label for="getKey"><a style={props.mode !== 'MP2T' ? { color: 'grey' } : {}}>Select Key File</a></label>
 				<a onClick={props.toggleHex}>{props.showHex ? 'Hide Hex Input' : 'Paste Hex Values'}</a>
 				<a onClick={props.togglePreview}>{props.showVideo ? 'Hide Preview' : 'Show Preview'}</a>
 				<a onClick={props.changeViewMode}>{props.viewMode ? 'Show Tree View' : 'Show Multiview'}</a>
