86481180ee2e10373315353244df9a1cd8289aef
Bill Farmilo <bill@farmilostrategy.com>
2022-01-14 17:36:45 -0500

    add BIF image viewer component

--- src/components/video/index.js
+++ src/components/video/index.js
@@ -2,6 +2,16 @@ import { h } from 'preact';
 import style from './style';
 
 const Video = (props) => {
+    // if it's a bif, props.data is an array of {timestamp, src}
+    if (props.mimeType === 'bif') return (
+        <div style={{ display: 'flex', flexWrap: 'wrap', marginTop: '60px' }}>
+            {props.data.map(trickImage => (<div key={trickImage.timestamp} style={{ margin: '0 5px' }}>
+                <img src={trickImage.src} />
+                <div style={{ justifyContent: 'center' }}>Time={trickImage.timestamp}s</div>
+            </div>)
+            )}
+        </div>
+    );
     return (
         <video
             src={`data:video/${props.mimeType}; base64,${props.data}`}
@@ -10,7 +20,7 @@ const Video = (props) => {
             controls
             onError={props.handleEncrypted}
         />
-    )
+    );
 };
 
 export default Video;
